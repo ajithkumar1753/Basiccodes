6508b79f28692d1b52e43e883b7087ec901c578c
ajithkumar1753 <ajithkumar.tech1753@gmail.com>
2024-06-19 14:48:34 +0530

    Basic code

---
+++ src/test/java/org/basicgitpractice/Basiccode.java
@@ -0,0 +1,32 @@
+package org.basicgitpractice;
+
+public class Basiccode {
+
+	
+	
+	public void firstcode() {
+		
+		System.out.println("1st code in eclipse");
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		Basiccode a = new Basiccode();
+		
+		a.firstcode();
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
